18fd4f38bbdaf04b2d33ae5b35b216da923cc64b
连铮 <97948437+lianzheng14@users.noreply.github.com>
2023-08-04 16:17:52 +0800

    Add files via upload
    抛出异常，try...catch和finally的使用。

--- Main.java
+++ Main.java
@@ -1,56 +1,15 @@
-interface Animal {
-   void shout();
-
-    }
-
-interface Action extends Animal{    //接口的方式   implements实现
-    void eat();
-        }
-//abstract  class Action  {     //抽象类的方式  extends继承
-//    public abstract void eat();
-//}
-
-
-class Dog implements Action{
-            @Override
-            public void shout() {
-                System.out.println("汪汪汪");
-            }
-
-            @Override
-            public void eat() {
-                System.out.println("吃骨头");
-            }
-
-}
-class Cat implements Action{
-    @Override
-    public void shout() {
-        System.out.println("喵喵瞄");
-    }
-
-    @Override
-    public void eat() {
-        System.out.println("吃鱼");
-    }
-
-}
-public class Main {
-    public static void animalShout(Animal an) {
-        an.shout();
-    }
-
+public class Main{
     public static void main(String[] args) {
-        Animal a1 = new Dog();
-        Animal a2 = new Dog();
-        System.out.println(a1.equals(a2));
-        System.out.println(a1.toString().equals(a2.toString()));
-        System.out.println(a1.toString());
-        System.out.println(a2.toString());
-        String a4="haha";
-        String a3="haha";
-        System.out.println(a3.equals(a4));
+        try{
+            int x=5/0;
+        }catch (RuntimeException e){
+            System.out.println(e.getMessage());
+//            return;            //return用于结束当前方法，这里是main方法，也就是异常处理完毕后的代码不会执行
+        }
+        finally {
+            System.out.println("执行finally语句");
+        }
+        System.out.println("异常处理完毕后的代码");
     }
-}
-
+        }
 
