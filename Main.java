0a68536e9127cd39d0836f9b393e655870b39131
连铮 <97948437+lianzheng14@users.noreply.github.com>
2023-08-04 16:26:03 +0800

    Update Main.java
    add some attention

--- Main.java
+++ Main.java
@@ -5,6 +5,7 @@ public class Main{
         }catch (RuntimeException e){
             System.out.println(e.getMessage());
 //            return;            //return用于结束当前方法，这里是main方法，也就是异常处理完毕后的代码不会执行
+// System.exit(0);         //finally语句将不会执行，这是表示退出当前的java虚拟机，java虚拟机停止了，一切代码都不会执行
         }
         finally {
             System.out.println("执行finally语句");
